3de0a15f634053d969d853a341e2e87e5dfe9d66
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-10-11 17:25:56 -0600

    feat cambiar DE/best/1/exp por DE/rand/2/exp

--- Actividad_6_Evolucion_Diferencial/Evolucion_diferencial.py
+++ Actividad_6_Evolucion_Diferencial/Evolucion_diferencial.py
@@ -84,9 +84,9 @@ def DE():
 
 
 # ----------------------------------------------------
-# ----------------> DE BEST 1 BIN <-------------------
+# ----------------> DE rand 2 exp <-------------------
 # ----------------------------------------------------
-def best():
+def rand():
     for n in range(G):
 
         for i in range(N):
@@ -98,10 +98,20 @@ def best():
             r2 = r1
             while r2 == r1 or r2 == i:
                 r2 = np.random.randint(N)
+            
+            r3 = r2
+            while r3 == r2 or r3 == r1 or r3 == i:
+                r3 = np.random.randint(N)
 
-            best = np.argmin(fitness)
+            r4 = r3
+            while r4 == r3 or r4 == r2 or r4 == r1 or r4 == i:
+                r4 = np.random.randint(N)
 
-            v = x[:, best] + F * (x[:, r1] - x[:, r2])
+            r5 = r4
+            while r5 == r4 or r5== r3 or r5 == r2 or r5 == r1 or r5 == i:
+                r5 = np.random.randint(N)
+
+            v = x[:, r1] + F * (x[:, r2] - x[:, r3]) + F * (x[:,r4] - x[:,r5])
 
             # Recombinación
             u = np.zeros(D)
@@ -172,7 +182,7 @@ def current_to_best():
 
         fx_plot[n] = np.min(fitness)
 
-current_to_best()
+rand()
 igb = np.argmin(fitness)
 
 print("Mínimo global en x=", x[0, igb], " y=",
