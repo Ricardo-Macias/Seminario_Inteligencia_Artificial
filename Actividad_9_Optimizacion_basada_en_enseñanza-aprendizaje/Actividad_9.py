6846e3977fc7d1d03524f90031252a41511af10e
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-11-05 12:49:28 -0600

    feat: agregar funciones objetivo

--- Actividad_9_Optimizacion_basada_en_enseñanza-aprendizaje/Actividad_9.py
+++ Actividad_9_Optimizacion_basada_en_enseñanza-aprendizaje/Actividad_9.py
@@ -1,11 +1,22 @@
-import matplotlib.pyplot as plt
+from Actividad_5_Optimizacion_por_enjambre_de_particulas import Plot_Surf
+from Actividad_5_Optimizacion_por_enjambre_de_particulas import Plot_Contour
 import numpy as np
-from Plot_Surf import plot_surf
-from Plot_Contour import plot_contour
+import matplotlib.pyplot as plt
+import sys
+sys.path.append(
+    "D:\\Archivos\\Practicas\\7_Semestre\\Seminario_Inteligencia_Artificial")
 
 
-def f(x, y): return (x-2)**2 + (y-2)**2
-# f = lambda x, y: 10*2 + x**2 + y**2 - 10*np.cos(2*np.pi*x) - 10*np.cos(2*np.pi*y)
+def Griewank(x, y): return ((x**2/4000)+(y**2/4000)) - \
+    (np.cos(x) * np.cos(y/np.sqrt(2))) + 1
+
+
+def Rastrigin(x, y): return 10*2 + x**2 + y**2 - 10 * \
+    np.cos(2*np.pi*x) - 10*np.cos(2*np.pi*y)
+
+
+def Sphere(x, y): return x**2 + y**2
+def McCormick(x, y): return np.sin(x+y) + (x-y) ** 2 - 1.5*x + 2.5*y + 1
 
 
 xl = np.array([-5, -5])
@@ -20,10 +31,9 @@ fitness = np.zeros(N)
 
 for i in range(N):
     x[:, i] = xl + (xu - xl) * np.random.rand(D)
-    fitness[i] = f(x[0, i], x[1, i])
+    fitness[i] = Griewank(x[0, i], x[1, i])
 
 for g in range(G):
-    plot_contour(f, x, xl, xu)
 
     for i in range(N):
         # Teacher phase
@@ -37,7 +47,7 @@ for g in range(G):
 
             c[j] = x[j, i] + r * (x[j, t] - Tf * x_mean)
 
-        fc = f(c[0], c[1])
+        fc = Griewank(c[0], c[1])
 
         if fc < fitness[i]:
             x[:, i] = c
@@ -68,5 +78,6 @@ for g in range(G):
 igb = np.argmin(fitness)
 
 print("Mínimo global en x=", x[0, igb], " y=",
-      x[1, igb], " f(x,y)=", f(x[0, igb], x[1, igb]))
-plot_surf(f, x, xl, xu, igb)
+      x[1, igb], " f(x,y)=", Griewank(x[0, igb], x[1, igb]))
+Plot_Surf.plot_surf(Griewank, x, xl, xu, igb)
+Plot_Contour.plot_contour(Griewank, x, xl, xu)
